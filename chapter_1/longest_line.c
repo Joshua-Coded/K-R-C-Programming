10f74ffe119831a198fe3593444c91f759674b6f
Joshbam Joshwilly <56293324+Joshbam-joshwilly@users.noreply.github.com>
2023-04-12 01:38:07 +0200

    Update longest_line.c

--- chapter_1/longest_line.c
+++ chapter_1/longest_line.c
@@ -1,58 +1,65 @@
 #include <stdio.h>
-#define MAXLINE 1000 // MAXIMUM INPUT LINE SIZE
+#define MAXLINE 1000 // Maximum input line size
 
-
-// prototypes for all our functionS
-
-size_t getline(char line[], size_t maxline);
+// Function prototypes
+int getLine(char line[], int maxline);
 void copy(char to[], char from[]);
 
-// print longest input lines
-
+// Main function
 int main()
 {
-  int len;
-  int max;
-  char line[MAXLINE];
-  char longest[MAXLINE];
+  int len, max;
+  char line[MAXLINE], longest[MAXLINE];
 
   max = 0;
 
-  while ((len = getline(line, MAXLINE)) > 0)
-    if (len > max) {
+  while ((len = getLine(line, MAXLINE)) > 0)
+  {
+    if (len > max) 
+    {
       max = len;
       copy(longest, line);
     }
+  }
 
-  if (max  > 0)
+  if (max > 0)
+  {
     printf("%s", longest);
+  }
+
   return 0;
 }
 
-
-// getline: read a line into s, return lenght
-
-size_t getline(char s[], size_t *lim)
+// Function to read a line into s and return length
+int getLine(char s[], int lim)
 {
   int c, i;
 
-  for(i = 0; i < lim -1 && (c = getchar()) != EOF && c != '\n'; i++)
+  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+  {
     s[i] = c;
-
-  if (c == '\n') {
-    s[i] = c;
-    ++i;
   }
+
+  if (c == '\n') 
+  {
+    s[i] = c;
+    i++;
+  }
+
   s[i] = '\0';
+
   return i;
 }
 
-// copy from into to;
-
+// Function to copy from into to
 void copy(char to[], char from[])
 {
   int i;
-  i = 0;
-  while ((to[i] = from[i]) != '\n')
-    ++i;
+
+  for (i = 0; from[i] != '\0'; i++)
+  {
+    to[i] = from[i];
+  }
+
+  to[i] = '\0';
 }
