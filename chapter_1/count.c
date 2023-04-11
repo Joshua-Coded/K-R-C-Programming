039d313928b4c82f8a1ce9ea9bc4cf94b758613d
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-11 17:51:47 +0200

    version 2

---
+++ chapter_1/count.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+  // this program counts character inputs; version 1
+
+  /*
+  long nc;
+  nc = 0;
+
+  while (getchar() != EOF)
+    {
+      nc++;
+      printf("%ld\n", nc);
+    }
+  */
+  double nc;
+  for(nc = 0; getchar() != EOF; ++nc)
+    {
+      ;
+       printf("%.0f\n", nc);
+    }
+}
